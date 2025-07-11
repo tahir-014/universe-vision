3b7cf09c66b8a2c92f0569b47102a23b37ebe1de
tahir-014 <tahirhussainhats0@gmail.com>
2025-07-11 23:48:10 +0530

    Initial commit for Universe Vision

---
+++ scripts/train_model.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import cifar10
+import matplotlib.pyplot as plt
+
+# Load CIFAR-10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# CNN Model
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train
+model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+# Save
+model.save("../model/universe_vision.h5")
